7e0f36b6487986af5b1ce56646c1fbc9856c0b0d
morgan1982 <alexandrisanas@gmail.com>
2017-10-30 21:56:04 +0200

    module to handle the routes

--- server.js
+++ server.js
@@ -1,21 +1,4 @@
 const http = require('http');
-const module1 = require('./module1');
-const module2 = require('./module2');
-const fs = require('fs'); // filesystem module
+const app =  require('./app');
 
-
-http.createServer( (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('index.html', null, (error, data) => {
-    if (error) {
-      res.writeHead(404);
-      res.write("file not found..")
-    } else {
-      res.write(data);
-    }
-    res.end();
-  }); //callback will run when it finishes reading the file(inde.html)
-  // res.write(module2.myVariable);
-  // module2.myFunction();
-
-}).listen(3000);
+http.createServer(app.handleRequest).listen(3000);
